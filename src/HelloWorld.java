f8f3e305bd891ba66e2fed127f6a2538253e20b8
BharatChaparala <bharat.ch@gmail.com>
2017-02-26 18:22:06 +0530

    version2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hellow World");
+		
+		System.out.println("Welcome to world of Java programs");
+		
 	}
 
 }
